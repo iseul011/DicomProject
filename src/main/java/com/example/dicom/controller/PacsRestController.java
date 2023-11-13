fea4409e9dcaca8a42a9fa03127eb116a5885273
phs <quaj628@gmail.com>
2023-11-13 17:48:45 +0900

    update date function 11/10 17:50 ParkHyunser

--- src/main/java/com/example/dicom/controller/PacsRestController.java
+++ src/main/java/com/example/dicom/controller/PacsRestController.java
@@ -21,6 +21,36 @@ public class PacsRestController {
     private final PacsImagetabRepository pacsImagetabRepository;
     private final PacsStudytabRepository pacsStudytabRepository;
 
+    @GetMapping("/search/PacsSeriestab")
+    public List<PacsSeriestab> getPacsSeriestab(@RequestParam int studykey) {
+
+        List<PacsSeriestab> pacsSeriestab = pacsSeriestabRepository.findAllByStudykey(studykey);
+        return pacsSeriestab;
+    }
+
+    @GetMapping("/search/PacsStudytab")
+    public List<PacsStudytab> getPacsStudytab() {
+
+        List<PacsStudytab> pacsStudytab = pacsStudytabRepository.findAll();
+
+        return pacsStudytab;
+    }
+
+    @GetMapping("/search/PacsImagetab/search")
+    public List<PacsImagetab> getPacsImagetab(@RequestParam int studykey, @RequestParam int serieskey) {
+
+        List<PacsImagetab> pacsImagetab = pacsImagetabRepository.findAllByStudykeyAndSerieskey(studykey, serieskey);
+        return pacsImagetab;
+    }
+
+    @DeleteMapping("/delete")
+    public void Delete(@RequestBody List<String> pid) {
+
+        for (int i = 0; i < pid.size(); i++) {
+            pacsStudytabRepository.deleteByPid(pid.get(i));
+        }
+    }
+
     //여기서 부터 써치
     @GetMapping("/search/PacsStudytab/{pid}")
     public List<PacsStudytab> getPacsStudytabByPid(@PathVariable String pid) {
@@ -58,37 +88,28 @@ public class PacsRestController {
     }
     //여기가 써치 끝
 
-    @GetMapping("/search/PacsSeriestab")
-    public List<PacsSeriestab> getPacsSeriestab(@RequestParam int studykey) {
+    //날짜 시작
+    @GetMapping("/search/PacsStudytab/oneAgo")
+    public List<PacsStudytab> oneAgo() {
 
-        List<PacsSeriestab> pacsSeriestab = pacsSeriestabRepository.findAllByStudykey(studykey);
-        return pacsSeriestab;
-    }
-
-    @GetMapping("/search/PacsStudytab")
-    public List<PacsStudytab> getPacsStudytab() {
-
-        List<PacsStudytab> pacsStudytab = pacsStudytabRepository.findAll();
-
+        List<PacsStudytab> pacsStudytab = pacsStudytabRepository.oneAgo();
         return pacsStudytab;
     }
 
-    @GetMapping("/search/PacsImagetab/search")
-    public List<PacsImagetab> getPacsImagetab(@RequestParam int studykey, @RequestParam int serieskey) {
+    @GetMapping("/search/PacsStudytab/threeAgo")
+    public List<PacsStudytab> threeAgo() {
 
-        List<PacsImagetab> pacsImagetab = pacsImagetabRepository.findAllByStudykeyAndSerieskey(studykey, serieskey);
-        return pacsImagetab;
+        List<PacsStudytab> pacsStudytab = pacsStudytabRepository.threeAgo();
+        return pacsStudytab;
     }
 
-    @DeleteMapping("/delete")
-    public void Delete(@RequestBody List<String> pid) {
+    @GetMapping("/search/PacsStudytab/oneWeekAgo")
+    public List<PacsStudytab> oneWeekAgo() {
 
-        for (int i = 0; i < pid.size(); i++) {
-            pacsStudytabRepository.deleteByPid(pid.get(i));
-        }
+        List<PacsStudytab> pacsStudytab = pacsStudytabRepository.oneWeekAgo();
+        return pacsStudytab;
     }
-
-
+    //날짜 끝
 
 //    @GetMapping(value = "/download", produces = MediaType.APPLICATION_OCTET_STREAM_VALUE)
 //    public ResponseEntity<Resource> downloadFile(@RequestHeader("user-Agent") String userAgent, String fileName) {
--- src/main/java/com/example/dicom/domain/PacsStudytabRepository.java
+++ src/main/java/com/example/dicom/domain/PacsStudytabRepository.java
@@ -6,23 +6,46 @@ import org.springframework.data.jpa.repository.Query;
 import java.util.List;
 
 public interface PacsStudytabRepository extends JpaRepository<PacsStudytab, Integer> {
-
     String Reportstatus = "select * from STUDYTAB where reportstatus = ?";
-    String threeFindAll = "select * from STUDYTAB where  pname= ? or pid=? or reportstatus=?";
+    String threeFindAll = "select * from STUDYTAB where pname= ? or pid=? or reportstatus=?";
+
+    String oneAgo = "SELECT *\n" +
+            " FROM STUDYTAB\n" +
+            " WHERE studydate >= TRUNC(SYSDATE) - 1\n" +
+            "  AND studydate < TRUNC(SYSDATE)";
+
+    String threeAgo = "SELECT *\n" +
+            " FROM STUDYTAB\n" +
+            " WHERE studydate >= TRUNC(SYSDATE) - 3\n" +
+            "  AND studydate < TRUNC(SYSDATE)";
+
+    String oneWeekAgo = "SELECT *\n" +
+            " FROM STUDYTAB\n" +
+            " WHERE studydate >= TRUNC(SYSDATE) - 7\n" +
+            "  AND studydate < TRUNC(SYSDATE)";
 
     List<PacsStudytab> findByStudykey(int studykey);
 
-    List<PacsStudytab> findAllByPid(String pid);
-
+    // 써치
+    List<PacsStudytab> findAllByPid(String pid); // 세부조회에도 씀
     List<PacsStudytab> findAllByPname(String pname);
-
     @Query(value = Reportstatus, nativeQuery = true)
     List<PacsStudytab> ReportstatusAll(String reportstatus);
-
     List<PacsStudytab> findAllByPidAndPname(String pid, String pname);
-
     @Query(value = threeFindAll, nativeQuery = true)
     List<PacsStudytab> threeFindAll(String pid, String pname, String reportstatus);
+    // 써치 끝
+
+    //날짜
+    @Query(value = oneAgo, nativeQuery = true)
+    List<PacsStudytab> oneAgo();
+
+    @Query(value = threeAgo, nativeQuery = true)
+    List<PacsStudytab> threeAgo();
+
+    @Query(value = oneWeekAgo, nativeQuery = true)
+    List<PacsStudytab> oneWeekAgo();
+    //날짜 끝
 
     void deleteByPid(String pid);
 
