edb28684e8633bd6cb299165d6d13b7e6e1905f8
JJH <j3261221@gmail.com>
2023-11-14 17:53:29 +0900

    Update worklist 11/14 17:53 주종현

--- src/main/java/com/example/dicom/domain/PacsStudytabRepository.java
+++ src/main/java/com/example/dicom/domain/PacsStudytabRepository.java
@@ -7,19 +7,8 @@ import org.springframework.data.jpa.repository.Query;
 import java.util.List;
 
 public interface PacsStudytabRepository extends JpaRepository<PacsStudytab, Integer> {
-    String Reportstatus = "select * from STUDYTAB where reportstatus = ?";
 
-    String threeFindAll = "select * from STUDYTAB where pname= ? or pid=? or reportstatus=?";
-
-    String oneAgo = "SELECT *\n" +
-            " FROM STUDYTAB\n" +
-            " WHERE studydate >= TRUNC(SYSDATE) - 1\n" +
-            "  AND studydate < TRUNC(SYSDATE)";
-
-    String threeAgo = "SELECT *\n" +
-            " FROM STUDYTAB\n" +
-            " WHERE studydate >= TRUNC(SYSDATE) - 3\n" +
-            "  AND studydate < TRUNC(SYSDATE)";
+    List<PacsStudytab> findAllByPid(String pid);
 
     List<PacsStudytab> findByPid(String pid, Sort sort);
 
