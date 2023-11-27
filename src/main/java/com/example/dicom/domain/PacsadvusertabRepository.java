b1714bb7279df96ebf5b847a2f7e942c540443ff
82104 <yi10286@gmail.com>
2023-11-27 17:52:25 +0900

    modify: 시큐리티 기능에 대한 설명 추가 11-27 17:50 이지훈

--- src/main/java/com/example/dicom/domain/PacsadvusertabRepository.java
+++ src/main/java/com/example/dicom/domain/PacsadvusertabRepository.java
@@ -4,10 +4,12 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
 
-
+// JpaRepository를 상속하여 Pacsadvusertab 엔티티를 조작하는데 필요한 메소드를 상속받습니다.
 public interface PacsadvusertabRepository extends JpaRepository<Pacsadvusertab, String> {
+
+    // 사용자명과 비밀번호를 이용하여 사용자를 조회하는 메소드
     Pacsadvusertab findByUseridAndPassword(String userid, String password);
+
+    // 사용자명을 이용하여 사용자를 조회하는 메소드
     Optional<Pacsadvusertab> findByUserid(String userid);
-
-
 }
