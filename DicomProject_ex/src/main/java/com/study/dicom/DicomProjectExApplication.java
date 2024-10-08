d3da6c2f6ab4bd35dce3820f7508a26dea28444d
iseul011 <kayira011@naver.com>
2024-10-08 18:31:01 +0900

    .

---
+++ DicomProject_ex/src/main/java/com/study/dicom/DicomProjectExApplication.java
@@ -0,0 +1,13 @@
+package com.study.dicom;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DicomProjectExApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DicomProjectExApplication.class, args);
+	}
+
+}
